37113ca80366d1663595b59e8de81b33c08c998a
lasanjin <sanjinslavnic@gmail.com>
2020-01-19 10:18:45 +0100

    Updates run script

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@
 g++ -c main.cpp -I resources/SFML-2.5.1/include
 g++ main.o -o tetris -L resources/SFML-2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system
 export LD_LIBRARY_PATH=resources/SFML-2.5.1/lib && ./tetris
+
+rm *.o && rm tetris
